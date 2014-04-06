87a6d6e5d2eaa648c3866af3f40d597538ac355a
darksoullock <SV171094@ya.ru>
2014-04-06 05:42:23 +0300

    list of files
    filelist saves into std::list

--- BackUp/filelist.h
+++ BackUp/filelist.h
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <windows.h>
-void printFiles(char * path)
+#include <list>
+using namespace std;
+void GetFileList(const char * cpath, list<char*> * r)
 {
-    DWORD a = GetFileAttributes(path);
-    if (a!=INVALID_FILE_ATTRIBUTES)
+    char path[MAX_PATH];
+	{							//path should ends with '\\'
+		int l = strlen(cpath);
+        strcpy(path,cpath);
+        if (path[l-1]!='\\')
+        {
+            path[l]='\\';       //strcat
+            path[l+1]=0;
+        }
+    }
+    if (GetFileAttributes(path)!=INVALID_FILE_ATTRIBUTES)
     {
         SetCurrentDirectory(path);
         WIN32_FIND_DATA fd;
@@ -20,13 +31,20 @@ void printFiles(char * path)
                     {
                         char buf[MAX_PATH];
                         strcpy(buf,path);
-                        strcat(buf,"\\");
                         strcat(buf,fd.cFileName);
-                        printFiles(buf);
+                        GetFileList(buf,r);
                     }
                 }
                 else
-                    std::cout << path << '\\' << fd.cFileName << std::endl;
+                {
+                    if (attr&FILE_ATTRIBUTE_ARCHIVE)    //only changed files need
+                    {
+                        char *buf = new char[MAX_PATH];
+                        strcpy(buf,path);
+                        strcat(buf,fd.cFileName);
+                        r->push_front(buf);
+                    }
+                }
                 SetCurrentDirectory(path);
                 b = FindNextFile(h,&fd);
             }
@@ -34,3 +52,12 @@ void printFiles(char * path)
         CloseHandle(h);
     }
 }
+/*int main()
+{
+    list<char*> *a = new list<char*>();
+    GetFileList("F:\\",a);
+    list<char*>::iterator it;
+    for (it = a->begin(); it!=a->end(); ++it)
+        cout << *it << endl;
+    return 0;
+}*/
